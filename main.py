dabac5e7e5983055f6515870b2f14e1b07233c9d
Naumov Dmitry <deemanaumov@gmail.com>
2023-06-24 12:47:46 +0300

    v1

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from tkinter import *
 import glob
 import winsound
+from functools import partial
 
 root = Tk()
 root.title("krutaya dota2")
 root.geometry("720x480")
 root.resizable(0, 0)
 root["bg"] = "lavender"
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
 
 imgs = []
 imgsFiles = []
@@ -15,23 +26,26 @@ for imgFile in glob.glob("./img/heroes/*.png"):
     imgsFiles.append(imgFile)
 
 
-def getSound(pos):
-    # winsound.PlaySound(imgsFiles[pos].replace("img", "sound").replace(".png", ".wav"), winsound.SND_ASYNC)
-    print(pos)
+soundsFiles = []
+for soundFile in glob.glob("./sound/heroes/*.wav"):
+    soundsFiles.append(soundFile)
 
+def getSound(audioFile):
+    winsound.PlaySound(audioFile, winsound.SND_ASYNC)
 
 xs = 26
 ys = 30
-c = 0
+heroBtns = []
 
 for i in range(0, len(imgs)):
-    hero = Button(image=imgs[i], command=lambda: getSound(imgFile))
-    hero.place(x=xs, y=ys)
+    heroBtns.append(Button(image=imgs[i], command=partial(getSound, soundsFiles[i])))
+
+for btn in heroBtns:
+    btn.place(x=xs, y=ys)
     xs += 178
     if xs > 560:
         xs = 26
         ys += 102
-
 
 if __name__ == "__main__":
     root.mainloop()
